960aa6591a63a53d440470322f9257d7d98d5f12
Ihyun Song <ju9699s@gmail.com>
2021-09-29 21:20:11 +0900

    Time: 24 ms (96.68%), Space: 14.1 MB (94.35%) - LeetHub

--- decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
+++ decrypt-string-from-alphabet-to-integer-mapping/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -1,22 +1,15 @@
 class Solution:
     def freqAlphabets(self, s: str) -> str:
         answer = ''
-        anslist = []
         
         i = len(s)-1
         
         while i>=0:
             if s[i]=='#':
-                anslist.append(int(s[i-2:i]))
+                answer = chr(int(s[i-2:i])+96) + answer
                 i-=3
             else:
-                anslist.append(int(s[i]))
+                answer = chr(int(s[i])+96) + answer
                 i-=1
-                
-        anslist.reverse()
-        
-        for num in anslist:
-            answer += chr(num+96)
         
         return answer
-        
