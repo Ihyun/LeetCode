169264282190c679aaf9d8e2a0543f5c021b42a0
Ihyun Song <ju9699s@gmail.com>
2021-11-16 03:18:32 +0900

    Time: 24 ms (95.95%), Space: 14.2 MB (42.07%) - LeetHub

--- fibonacci-number/fibonacci-number.py
+++ fibonacci-number/fibonacci-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def fib(self, n: int) -> int:
+        sol = [0, 1]
+        
         if n==0:
             return 0
         elif n==1:
             return 1
-        
-        return self.fib(n-1)+self.fib(n-2)
-        
+        else:
+            for i in range(n-1):
+                sol.append(sol[-1]+sol[-2])
+            
+            return sol[-1]
